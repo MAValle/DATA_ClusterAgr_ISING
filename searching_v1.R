9382af01082e1fb78a54ada987a6c90089b38531
MAValle <mauvalle@gmail.com>
2019-06-09 18:21:54 -0400

    upgrade Jun9, 2019

--- searching_v1.R
+++ searching_v1.R
@@ -13,7 +13,17 @@
 
 #Notas:
 # 06-jun-19: creation
+# 09 jun: he creado la funcion energy_coupling_and_mst_search que hace de una vez la 
+#       subrutina energy_coupling_search y energy_mst_search y el resultado es una matriz
+#       con todas las energias de acoples y de mst para la agregacion de cada uno de los 
+#       nodos disponibles al vector de cluster v. 
 
+# Procedure before the algorithm
+# load de wb = matriz de canastas 179,610 X 25
+# load J matriz de acoples 256 X 25
+# create net = red completa de acoples con 25 nodos y 24X24/2 = 300 edges
+# create mst_g = red mst de la red de acoples en que dis = sqrt (-J + 3)
+# create nodo de partida.
 
 # Algoritmo:
 # 1. Se selecciona un vertice o nodo (o producto) de partida S = {nodo inicial}
@@ -47,7 +57,7 @@ library(igraph)
 source("entropies_functions.R")
 source("network_functions_v1.R")
 
-load('new_inferring_parameters_environment270219.RData')
+load('new_inferring_parameters_environment270219.RData') # load H and J parameters ising inferred
 # # # # # # # # # # # # # # # # # # # # CARGA de datos # # # # # # # # # # # # # # # # # # # # 
 
 
@@ -121,6 +131,59 @@ find_mst_barrier <- function(mst_g, v) {
 #E_mst <- find_mst_barrier(mst_g, v)
 # # # # # # # # # # # # # # # # # # # # # FIND Emst # # # # # # # # # # #  # # # # # # # # # #
 
+# # # # # # # # # # # # # # # # # # energy_coupling_search# # # # # # # # ## # # # # # # # # #
+# Esta funcion lo que hace es buscar el producto que incrementa la minima energia al cluster
+# dado o contenido en v.
+
+###### FUNCTION - subroutine coupling_and_mst_search
+# Created on Jun 7, 2019.
+
+# Lo que hace esta funcion es que para un vecttor de estado v (o cluster), se calculan la energia
+# de acoples y energia mst de la agregacion de un producto mas al vector n. El resultado es una
+# matriz con esas energias para cada uno de los restantes productos o nodos para postprocesamiento.
+# input: vector de cluster v
+#        tes = es un vector con los id de columnas de v que se utiliza despues. Siempre es el mismo
+#        option = "all" si queremos la matriz de resultados con todos las combinaciones o "one" si queremos solo el minimo.
+# output: item o nodo que produce la menor energia y la energia de acople correspondiente.
+# Nota: la matriz de acoples J debe estar cargada en memoria.
+energy_coupling_and_mst_search <- function(v, tes, option="one") {
+  node_names <- colnames(J)
+  ocupados <- which(v==1)    #ocupados = c(3,4)
+  a_probar <- tes[! tes %in% ocupados] # dejamos los cols id de v, que estan vacios con ceros.
+  a_probar_names <- node_names[!node_names %in% node_names[ocupados]] # nombre de los nodos que vamos a probar
+  almacen <- matrix(NA, ncol=5, nrow=length(a_probar)) # aqui vamos dejando las energias de acople y el producto correspondiente
+  almacen[,2] <- a_probar
+  almacen[,1] <- as.numeric(a_probar_names)
+  colnames(almacen) <- c("vertex_name", "vertex", "coupling_energy", "mst_distance", "mst_energy")
+  for (i in 1:length(a_probar)) {
+    v_temporal <- v # esto despues es el input
+    id <- a_probar[i]
+    v_temporal[id] <- 1
+    #print(v_temporal)
+    # subrutina para calcular la energia de acople
+    almacen[i,3] <- get_energy(v_temporal, mode="c")
+    # subrutina para calcular las distancias mst y energias mst
+    id_temporal <- which(v_temporal==1)
+    items <- as.factor(names[id_temporal])
+    items <- as.numeric(as.character(items))
+    r <- d_mst_distances(mst_g, vec = items)  
+    almacen[i,4] <- r[[1]]
+    almacen[i,5] <- r[[2]]
+  }
+  if (option == "one") {
+    mini <- which(almacen[,3] == min(almacen[,3])) # retorna el minimo de energia de acople
+    mini2 <- which(almacen[,5] == min(almacen[,5])) # retorna el minimo de energia mst 
+    return( list( min_of_coupling =  almacen[mini,], min_of_mstenergy = almacen[mini2,]) )
+  } else {
+    return(almacen)
+  }
+}
+# Ejemplo
+#min_energy_coupling_search(v = v, tes = tes, option = "one")
+#min_energy_coupling_search(v = v, tes = tes, option = "all")
+# En el caso de "all" el resultado es la energia de acople de vector de entrada v con cardinalidad
+# N, con los N-1 nodos posibles para combinar con v.
+# # # # # # # # # # # # # # # # # # energy_coupling_search# # # # # # # # ## # # # # # # # # #
 # # # # # # # # # # # # # # # # # # # # FUNCIONES # #  # # # # # # # # # # # # # # # # # # # # 
 
 
@@ -144,9 +207,53 @@ edg <- as_edgelist(mst_g, names = TRUE)
 # # # # # # # # # # # # # # # # # # # # # FIND MST # # # # # # # # # # # # # # # # # # # # # #
 
 
-# Starting
-v <- find_starting_vertex(mst_g) # basado en min alpha*Ec + (1-alpha)*Emst  v=80
-###### subroutine energy_coupling_search
+# Starting ------------------------07, jun, 2019
+# create the vector-clustering
+v <- matrix(0, ncol=ncol(wb), nrow=1)
+colnames(v) <- colnames(wb)
+
+# Create de initial node to start
+item <- find_starting_vertex(mst_g) # basado en min alpha*Ec + (1-alpha)*Emst  v=80
+i <- which(colnames(v) == item)
+v[i] <- 1
+Ec <- get_energy(v, mode="c") # funcion de entropies_functions.R / Matriz J de acoples debe estar cargada.
+tes <- seq(1:ncol(v)) # un vector con los id de columnas de v que se utiliza despues.
+
+# subrutina energy_coupling_search y subrutina y subrutina de energy_mst_search en la funcion energy_coupling_and_mst_search
+r_energies <- energy_coupling_and_mst_search(v = v, tes = tes, option = "all")
+
+
+# Step 4: sumar alpha*temp_Ec + (1-alpha)*temp_Ec para cada nodo y seleccionar nodo x que corresponda 
+#         al minimo de alphaX*Ec + (1-alpha)*Emst.
+alpha = 0.5
+ponderation <- alpha*r_energies[,4] + (1-alpha)*r_energies[,5]
+r_energies <-  cbind(r_energies, ponderation)
+min_id <- which(r_energies[,6] == min(r_energies[,6]))
+resultado <- r_energies[min_id, ]
+
+# Step 5. S = S Union {nodo x}
+v[resultado[2]] <- 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
